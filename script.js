6ff5d84e207e19fb26779615d757b6db033a2150
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2026-01-07 22:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll(".circle");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+let current=0;
+
+circles[current].classList.add("active");
+next.addEventListener("click", function () {
+	if(current < circles.length){
+	circles[current+1].classList.add("active");
+	circles[current].classList.remove("active");
+	current++;
+	}
+
+})
+
+prev.addEventListener("click", function () {
+	if(current > 0){
+	circles[current-1].classList.add("active");
+	circles[current].classList.remove("active");
+	current--;
+	}
+
+})
+
+
